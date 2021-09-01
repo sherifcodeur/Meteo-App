1a1e7374a8ab9808e7b8ef0c2315ea7734510040
Idrissi Ismael <techorigin@gmail.com>
2021-09-01 22:27:40 +0100

    premier commit app construite

---
+++ js/app.js
@@ -0,0 +1,85 @@
+
+
+const API = '2512f75f39d80de37ff31d19c7bd8fd5' ;
+let currentTime = new Date().getHours();
+
+const descriptionTemps = document.getElementById("meteo-jour--texte--description");
+const temperature = document.getElementById("meteo-jour--texte--temperature");
+const timezoneformeteo = document.getElementById("meteo-jour--texte--timezone");
+
+const imageweather = document.getElementById("logo");
+
+
+navigator.geolocation.getCurrentPosition(response=>{
+
+        
+
+        let lat = response.coords.latitude;
+
+        let long = response.coords.longitude;
+
+       console.log(lat,long);
+
+       fetchAPI(lat,long);
+
+},()=>{
+
+    // will show a message in the app saying they didn't accept so it will show another position (Paris france)
+
+    let lat = 48.8566;
+    let long = 2.3522;
+    showMessage();
+
+});
+
+
+function fetchAPI(latitude,longitude){
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&lang=fr&exclude=minutely&appid=${API}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+
+
+        
+
+        console.log(data);
+
+        descriptionTemps.innerText = data.current.weather[0].description;
+
+        temperature.innerText = `${Math.floor(data.current.temp)} ° C`  ;
+
+        timezoneformeteo.innerText = data.timezone;
+
+        let iconeName = data.current.weather[0].icon;
+
+        // on check si on a un icone de nuit et jour
+        let dayornight = "jour";
+        if(iconeName.includes("n")){
+            dayornight = "nuit";
+        }
+
+        imageweather.innerHTML = `<img src="/resources/${dayornight}/${data.current.weather[0].icon}.svg" alt="image de la meteo du jour"></img>`
+
+        
+
+        
+
+        futurHours();
+
+        
+
+
+    }).catch();
+
+}
+
+function futurHours(){
+
+    for(i=0;i<21;i+=3){
+
+        console.log("hours",i);
+    }
+
+}
+
+
